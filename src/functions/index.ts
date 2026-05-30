37e35db9d378d80b85bf66e8a90e6ff31d65624a
Betpro <213392956+bpexch1@users.noreply.github.com>
2026-05-31 03:11:14 +0500

    chore: export local fetch functions if available, fallback to superdevClient

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,10 +1,26 @@
-
 import { superdevClient } from "@/lib/superdev/client";
 
 export const handleTransaction = superdevClient.functions.handleTransaction;
-export const fetchBetfairEvents = superdevClient.functions.fetchBetfairEvents;
 export const settleBets = superdevClient.functions.settleBets;
 export const getLiveOdds = superdevClient.functions.getLiveOdds;
 export const getCricketScore = superdevClient.functions.getCricketScore;
 export const oddsEngine = superdevClient.functions.oddsEngine;
-export const fetchAtdCricketHome = superdevClient.functions.fetchAtdCricketHome;
+
+// Use local fetch implementations when available
+try {
+  // Dynamic import to avoid build-time errors if files don't exist yet
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const atd = require('./fetch-atd-cricket-home').default;
+  const betfair = require('./fetch-betfair-events').default;
+  export const fetchAtdCricketHome = atd;
+  export const fetchBetfairEvents = betfair;
+} catch (err) {
+  // Fallback to superdevClient functions if local modules aren't present
+  // This keeps existing behavior while enabling local overrides
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  export const fetchAtdCricketHome = superdevClient.functions.fetchAtdCricketHome;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  export const fetchBetfairEvents = superdevClient.functions.fetchBetfairEvents;
+}
