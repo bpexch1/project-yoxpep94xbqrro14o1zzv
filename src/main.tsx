ff7accc5aeddd4a33968b67ec4d94bc9de0d1343
Betpro <213392956+bpexch1@users.noreply.github.com>
2026-06-30 03:37:55 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-// YE LINE SAHI HONI CHAHIYE
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
