8fed176e6e6ff6674d184aa6287f9c1cba8ab243
Betpro <213392956+bpexch1@users.noreply.github.com>
2026-06-29 06:06:36 +0500

    Update index.ts

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -11,20 +11,20 @@ export const User = {
 
 export const Client = {
   list: (options?: any) => core.list("Client", options),
-  filter: (query: any) => core.filter("Client", query)
+  filter: (query: any) => core.filter("Client", query),
 };
 
 export const Transaction = {
   list: (options?: any) => core.list("Transaction", options),
-  filter: (query: any) => core.filter("Transaction", query)
+  filter: (query: any) => core.filter("Transaction", query),
 };
 
 export const Match = {
   list: (options?: any) => core.list("Match", options),
-  filter: (query: any) => core.filter("Match", query)
+  filter: (query: any) => core.filter("Match", query),
 };
 
 export const Bet = {
   list: (options?: any) => core.list("Bet", options),
-  filter: (query: any) => core.filter("Bet", query)
+  filter: (query: any) => core.filter("Bet", query),
 };
